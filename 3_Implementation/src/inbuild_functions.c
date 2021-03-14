eb0d0bc16f115041cb564d6c5ba2e8760e85ab33
99003722 <78848562+99003722@users.noreply.github.com>
2021-03-14 13:30:11 +0530

    Update inbuild_functions.c

--- 3_Implementation/src/inbuild_functions.c
+++ 3_Implementation/src/inbuild_functions.c
@@ -1,12 +1,19 @@
 #include<inbuild_functions.h>
 #include<math.h>
 float Area_Of_Triangle(float side_1,float side_2,float side_3)
-{                float areaoftriangle0,areaoftriangle1;
+{               float areaoftriangle0,areaoftriangle1;
                  float s;
                  s = (side_1 + side_2 + side_3)/2;
                  areaoftriangle0 = (s*(s-side_1)*(s-side_2)*(s-side_3));
-                 areaoftriangle1=sqrt(areaoftriangle0);
-                 return areaoftriangle1;
+                 float temp,sqrt;
+                 sqrt=areaoftriangle0/2;
+                 temp=0;
+                  while(sqrt!=temp)
+                 {
+                        temp =sqrt;
+                           sqrt=(areaoftriangle0/temp +temp)/2;
+                }
+                 return sqrt;
 }
 float Perimeter_Of_Triangle(float side_1,float side_2,float side_3)
 {                float perimeteroftriangle;
